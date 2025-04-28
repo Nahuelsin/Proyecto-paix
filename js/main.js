4eb73d4a63449b4a884f42fd6df15b04028a939e
Nahuel Pias <nahuelpias988@gmail.com>
2025-04-28 19:20:17 -0300

    programacion del slider en js y creacion de nuevos divs contenedores para las imagenes

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const btnLeft = document.querySelector(".btnizq"),
+    btnDer = document.querySelector(".btnder"),
+    slider = document.querySelector("#slider"),
+    sliderSect = document.querySelectorAll(".slider_section");
+
+
+    /*    Funciones para mover a derecha e izq  */
+
+btnLeft.addEventListener("click",  e => moveToLeft())
+btnDer.addEventListener("click",  e => moveToRight())
+
+let operacion = 0,
+    counter = 0,
+    widthImg = 100 / sliderSect.length;
+
+function moveToRight(){
+    if (counter >= sliderSect.length-1){
+        counter = 0;
+        operacion = 0;
+        slider.style.transform = `translate(-${operacion}%)`
+    }else{
+        counter++;
+        operacion = operacion + widthImg;
+    slider.style.transform = `translate(-${operacion}%)`
+    slider.style.transition = "all ease .6s"
+    }
+    
+}
+
+function moveToLeft (){
+    counter--;
+    if (counter < 0){
+        
+    }else{
+        counter++;
+        operacion = operacion - widthImg;
+        slider.style.transform = `translate(-${operacion}%)`
+        slider.style.transition = "all ease .6s"
+    }
+}
